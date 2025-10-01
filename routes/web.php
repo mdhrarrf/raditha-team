529665a0e977cf5caff1310ba1b1ac3bbdbc1c8a
mdhrarrf <haidarmeeteng@gmail.com>
2025-10-01 08:47:37 +0700

    konflik selesai by haidar

--- routes/web.php
+++ routes/web.php
@@ -7,26 +7,21 @@ use App\Http\Controllers\UserController;
 
 // Halaman utama langsung ke dashboard
 Route::get('/', function () {
-<<<<<<< HEAD
     return view('dashboard'); // Breeze otomatis bikin view ini
 })->middleware(['auth'])->name('dashboard');
 
 // Resource untuk Buku Tamu dan Users
 Route::resource('buku-tamu', BukuTamuController::class)->middleware(['auth']);
 Route::resource('users', UserController::class)->middleware(['auth']);
-=======
-    return view('welcome');
-});
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
->>>>>>> main
 
 require __DIR__.'/auth.php';
